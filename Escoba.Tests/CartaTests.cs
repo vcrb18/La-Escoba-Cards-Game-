7c40bf9b04d21cbd2042dd66d2996e18f1ccb322
vcrb18 <vcrb@uc.cl>
2022-10-26 16:50:08 -0300

    Inicio de Tests

---
+++ Escoba.Tests/CartaTests.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using Xunit;
+
+namespace Escoba.Tests;
+
+public class CartaTests
+{
+    [Theory]
+    [InlineData("1", 1)]
+    [InlineData("2", 2)]
+    [InlineData("3", 3)]
+    [InlineData("4", 4)]
+    [InlineData("5", 5)]
+    [InlineData("6", 6)]
+    [InlineData("7", 7)]
+    [InlineData("Sota", 8)]
+    [InlineData("Caballo", 9)]
+    [InlineData("Rey", 10)]
+    public void ConvierteValorAInt_StringDelValorDebeConvertirse(string valor, int expected)
+    {
+        // Arrange
+        Carta carta = new Carta(valor)
+        // Act
+    }
+}
